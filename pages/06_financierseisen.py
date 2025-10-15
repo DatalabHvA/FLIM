caaf6c86c27228d06414ce0121e1a5186e4714ee
Jochanan-vdL <jochanan@circularities.com>
2025-10-15 10:49:59 +0200

    aanpassen titel

--- pages/06_financierseisen.py
+++ pages/06_financierseisen.py
@@ -34,6 +34,6 @@ hide_sidebar = """
 st.markdown(hide_sidebar, unsafe_allow_html=True)
 
 
-st.title('Financierseisen')
+st.title('Subsidies en financierseisen')
 st.page_link("Home.py", label="⬅ Terug naar Home")
 st.write(st.session_state["bubble_label"])
