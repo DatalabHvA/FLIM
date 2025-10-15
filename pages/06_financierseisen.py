d6cb0451d9a31d8833137328419689cbc6404749
Jochanan-vdL <jochanan@circularities.com>
2025-10-15 10:51:25 +0200

    Wijzigen titels

--- pages/06_financierseisen.py
+++ pages/06_financierseisen.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Financierseisen", layout="wide", initial_sidebar_state='collapsed')
+st.set_page_config(page_title="Subsidies en financierseisen", layout="wide", initial_sidebar_state='collapsed')
 
 hide_sidebar = """
     <style>
