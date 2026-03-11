ae74a29fe490d8186808865cd2d3227d62d61be5
Jochanan-vdL <jochanan@circularities.com>
2026-03-11 15:02:07 +0100

    Aanpassen outro

--- pages/06_subsidies.py
+++ pages/06_subsidies.py
@@ -137,4 +137,6 @@ Opschaling van een circulaire productlijn naar industrieel formaat om hout-compo
 
 
 st.subheader("Hulp nodig?")
-st.write("Het subsidielandschap kan complex zijn en verandert regelmatig. Heb je hulp nodig bij het vinden van passende regelingen of bij het opzetten van een aanvraag? Neem dan contact op met CBM. Zij kunnen meedenken over jouw projectidee, relevante subsidies signaleren en indien nodig doorverwijzen naar een subsidieadviseur. CBM is ook de mogelijkheid aan het verkennen om toegang te krijgen voor hun leden tot digitale subsidie- en AI-tools die helpen om snel te zien welke regelingen bij jouw plannen kunnen passen.")
+st.write("Het subsidielandschap kan complex zijn en verandert regelmatig. 
+Kun je wel hulp gebruiken bij het vinden van passende regelingen of bij het opstellen van een aanvraag? Neem dan contact op met CBM. Zij kunnen meedenken over jouw projectidee, relevante subsidies signaleren en indien nodig doorverwijzen naar een subsidieadviseur. 
+Vanuit de branche wordt momenteel ook de mogelijkheid verkend om toegang te krijgen tot digitale subsidie- en AI-tools die helpen om snel te bepalen welke regelingen bij jouw plannen kunnen passen.")
