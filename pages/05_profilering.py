fadc5d62cf10554247251330fc8749e57fc2b5f6
Jochanan-vdL <jochanan@circularities.com>
2026-03-11 15:51:51 +0100

    Nevermind afbeelding

--- pages/05_profilering.py
+++ pages/05_profilering.py
@@ -63,15 +63,6 @@ if page == "Overzicht":
     st.header("Strategisch en zichtbaar profileren voor een toekomstbestendige marktpositie")
     st.write("Hoe zichtbaar en geloofwaardig ben jij met je toekomstbestendige keuzes? Profilering gaat over hoe je jouw betere keuzes op dit gebied strategisch inzet om je marktpositie te versterken. Het draait om de samenhang tussen (1) bewijs, (2) transparantie, (3) ketenprofilering en (4) employer branding.")
 
-    c1, c2 = st.columns([6,2])
-    with c1: 
-        st.subheader("Hoe is het subsidielandschap opgebouwd?")
-        st.write("Subsidies sluiten vaak aan op een specifieke fase van jouw project. Onderstaand overzicht helpt je bepalen waar jij zit en waar je moet zoeken.")
-
-        st.image('assets/subsidies1.png')
-with c2: st.write("")
-    
-    
     with st.container(border = True):
         st.subheader("Relevantie en urgentie")
         st.write("Door je duurzame prestaties objectief te onderbouwen, hier open over te rapporteren en dit actief te benutten in je ketensamenwerking, bouw je aan een herkenbaar en onderscheidend duurzaam profiel. Dat profiel werkt door in je aantrekkelijkheid voor klanten, opdrachtgevers én talent. Profilering is daarmee geen communicatie achteraf, maar een strategische hefboom: het verbindt inhoudelijke verduurzaming met vertrouwen, markttoegang en concurrentievoordeel.")
