2882cadc01903a90df910c471d753253863bb263
Jochanan-vdL <jochanan@circularities.com>
2026-03-11 17:00:45 +0100

    Aanpassing onderttitel

--- pages/04_wet_regelgeving.py
+++ pages/04_wet_regelgeving.py
@@ -40,7 +40,7 @@ with st.sidebar:
 st.title('Wet- en regelgeving')
 
 if not ((ss.omzet_value == ">€50M") | (ss.medewerkers_value == "250+ fte")):
-    st.subheader("Waar wordt de meubelbranche de komende jaren mee geconfronteerd?")
+    st.subheader("Waar krijgt de branche de komende jaren mee te maken?")
     st.markdown("""
 De Europese Unie zet met nieuwe wetgeving stevig in op een zuiniger en slimmer gebruik van grondstoffen. 
 Hierdoor is het bewust omgaan met grondstoffen niet langer vrijblijvend, maar wordt het een wettelijke verplichting.
