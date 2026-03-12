955132ce9799c514b569bdab1e7b71404081aaa2
Jochanan-vdL <jochanan@circularities.com>
2026-03-12 08:01:25 +0100

    dikgedrukte titels

--- pages/05_profilering.py
+++ pages/05_profilering.py
@@ -68,7 +68,7 @@ with st.expander("Overzicht", expanded = True):
 
     #st.image('assets/Landing - profilering.png')
 
-with st.expander("Bewijslaag"):
+with st.expander("*Bewijslaag*"):
     st.header("Bewijslaag")
     st.write("Er zijn verschillende manieren om je als bedrijf voordeel te halen uit de stappen die je onderneemt op het gebied van ontwerp, grondstoffen en keuzes in jouw toeleveringsketen. Een van de stappen om hier betrouwbaar over te kunnen publiceren, is het creëren van een bewijslaag. Dit bewijs kan de vorm aannemen van certificeringen, labels, duurzaamheidsindices (alleen B2B) en onderbouwde impactmetingen van producten en materialen (EPD) met transparante doorrekeningen van de hele levenscyclus van producten (LCA’s). Daarmee maak je zichtbaar dat verduurzaming niet alleen een ambitie is, maar gebaseerd is op meetbare en verifieerbare resultaten. Dit vormt de basis voor betrouwbare en geloofwaardige communicatie richting klanten, opdrachtgevers, ketenpartners, financiers en medewerkers.")
     
