60b707e8a69fcfb30443dea11dbca38a34ff009c
Jochanan-vdL <jochanan@circularities.com>
2026-04-15 14:17:45 +0200

    Terugzetten

--- pages/01_Prijsstijgingen.py
+++ pages/01_Prijsstijgingen.py
@@ -1,15 +1,89 @@
-# Layout
-fig.update_layout(
-    xaxis_title="Jaar",
-    yaxis_title="Producentenprijsindex (2015 = 100)",
-    template="plotly_white",
-    **COMMON_LAYOUT,
+# pages/02_Leveringszekerheid_Map.py
+import numpy as np
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+import statsmodels.api as sm
+import sys
+
+sys.path.append("..")
+from widgets import *
+
+
+COMMON_LAYOUT = dict(
+    margin=dict(l=40, r=5, t=20, b=60),
+    xaxis=dict(showline=True, linecolor="black", mirror=True, tickfont=dict(size=11), title_standoff=10),
+    yaxis=dict(showline=True, linecolor="black", mirror=True, tickfont=dict(size=11), title_standoff=10),
+)
+CHART_HEIGHT = 320
+
+ss = st.session_state
+init_session_state()
+st.set_page_config(page_title="Prijsontwikkelingen", layout="wide")
+log_event("Prijsstijgingen", "page_load")
+
+st.markdown(
+    """
+    <style>
+      /* pull content up */
+      .block-container { padding-top: 0.9rem !important; }
+      /* compact header */
+      header[data-testid="stHeader"] { height: 1.2rem; }
+      [data-testid="stSidebarNav"] {display: none;}
+      [data-testid="stSidebar"] .block-container {
+          padding-top: 0 !important;
+      }
+
+    section[data-testid="stSidebar"] .block-container > div:first-child,
+    section[data-testid="stSidebar"] div[data-testid="stVerticalBlock"] > div:first-child {
+    margin-top: -60px !important;   /* <- adjust this number */
+    }
+
+    </style>
+    """,
+    unsafe_allow_html=True,
 )
 
-# Grafiek eerst tonen
-st.plotly_chart(fig, width='stretch')
+with st.sidebar:
+    st.page_link("Home.py", label="⬅ Terug naar Home")
 
-# Daarna de twee kolommen
+    st.header("Filters")
+    widget_materiaal_prijs()
+
+st.title("Prijsontwikkelingen")
+
+st.caption(f"Gefilterd op materiaal: **{ss.selected_materiaal_value}**")
+filtered_df = ss.prijzen_df[['Jaar',ss.selected_materiaal_value]].dropna()
+
+st.markdown("""
+    De prijsontwikkelingen zijn berekend met de **producentenprijsindex (PPI)** voor de gekozen materialen.
+    De PPI laat zien hoe de **gemiddelde prijzen veranderen die grondstoffen- of materiaalfabrikanten krijgen** voor hun producten. Het gaat dus om **prijzen bij de meubelproducent**, niet in de winkel.
+    Voor inkopers is de PPI belangrijk, omdat hij laat zien **hoeveel duurder of goedkoper materialen worden**. Dat helpt om de **inflatie** (algemene prijsstijgingen) en **prijsschommelingen** voor deze materialen in te schatten.
+    (bron: investingnomads.nl).
+            """)
+
+# Ensure datetime
+x_dt = pd.to_datetime(filtered_df['Jaar'])
+
+# Convert to numeric for regression
+x_num = x_dt.dt.year + (x_dt.dt.month - 1) / 12
+x_labels = x_dt.dt.strftime('%Y-%m')
+y = filtered_df[ss.selected_materiaal_value].astype(float)*100
+
+# Fit linear regression
+X = sm.add_constant(x_num)
+model = sm.OLS(y, X).fit()
+y_fit = model.predict(X)
+
+# Calculate residuals and std deviation
+residuals = y - y_fit
+std_dev = np.std(residuals)
+
+# Create ±1 std deviation band around the trendline
+ci_upper = y_fit + std_dev
+ci_lower = y_fit - std_dev
+
 col1, col2 = st.columns(2)
 with col1:
     with st.container(border=True):
@@ -56,3 +130,94 @@ with col2:
                 - Een hoge spreiding σ betekent volatiele markt met een hoge prijsonzekerheid.
                 - Vergelijk spreiding σ tussen materialen of grondstoffen om prijsonzekerheid zelf te beoordelen.
                 """)
+
+events = [
+    {"date": "2020-03-11", "label": "COVID-19", "color": "gray", "dash": "dot", "width": 3},
+    {"date": "2022-02-24", "label": "Invasie Oekraïne", "color": "green", "dash": "dash", "width": 4,},
+    #{"date": "2023-12-06", "label": "Pieter Jarig", "color": "purple", "dash": "solid", "width": 10,}
+]
+
+#Mogelijke dash opties: "solid", "dot", "dash", "longdash", "dashdot", "longdashdot"
+
+# Plot
+fig = go.Figure()
+
+# Original data
+fig.add_trace(go.Scatter(
+    x=x_labels,
+    y=y,
+    mode='lines+markers',
+    name=f"Historische prijs van {ss.selected_materiaal_value}",
+    line=dict(color='blue', width=3),
+    marker=dict(size=6)
+))
+
+# Trendline
+fig.add_trace(go.Scatter(
+    x=x_labels,
+    y=y_fit,
+    mode='lines',
+    name='Trendlijn',
+    line=dict(color='red', dash='dash')
+))
+
+# ±1σ prediction band
+fig.add_trace(go.Scatter(
+    x=list(x_labels) + list(x_labels[::-1]),
+    y=list(ci_upper) + list(ci_lower[::-1]),
+    fill='toself',
+    fillcolor='rgba(0,100,255,0.2)',
+    line=dict(color='rgba(255,255,255,0)'),
+    hoverinfo="skip",
+    name='Spreiding (±1σ)'
+))
+
+
+
+for e in events:
+    x = pd.to_datetime(e["date"])
+    fig.add_shape(
+        type="line",
+        x0=x, x1=x,
+        y0=0, y1=1,
+        xref="x", yref="paper",          # y in [0..1] over hele plothoogte
+        line=dict(color=e["color"], width=e["width"], dash=e["dash"]),
+        opacity=0.7,
+    )
+    fig.add_annotation(
+        x=x, y=1, xref="x", yref="paper",
+        text=e["label"],
+        showarrow=False,
+        xanchor="left",
+        yanchor="bottom",
+        bgcolor="white",
+        bordercolor=e["color"],
+        borderwidth=1,
+        opacity=0.9,
+    )
+
+# Layout
+fig.update_layout(
+    xaxis_title="Jaar",
+    yaxis_title="Producentenprijsindex (2015 = 100)",
+    template="plotly_white",
+    **COMMON_LAYOUT,
+)
+
+st.plotly_chart(fig, width='stretch')
+
+# Toelichtende tekst onder grafiek
+st.markdown("""
+   
+    Deze grafiek toont de volgende informatie voor de gekozen grondstof:
+    1.	de **trendlijn** (stijging of daling van de grondstof- of materiaalprijzen over een periode) en 
+    2.	de **schommeling** van de grondstof- of materiaalprijzen over een periode.
+
+    Voor meubelproductenten heeft de **trendlijn** betrekking op de prijsstrategiën, investeringsbeslissingen en het risicobeheer bij de productie van meubels. Bij een **stijgende trendlijn** betekent dit: 
+    - **Hogere grondstofkosten**. Meubelproducenten zijn **sterk afhankelijk van de gekozen grondstoffen**. Een **stijgende PPI** betekent dat deze **materialen duurder** worden. Dit verhoogt de productiekosten direct.
+    - Druk op marges en prijsbeleid. Als meubelmakers de **hogere kosten** niet volledig kunnen doorberekenen aan klanten, **daalt hun winstgevendheid**. Dit kan leiden tot prijsindexatie in contracten of het zoeken naar goedkopere materialen: 
+    - Invloed op vraag en concurrentie. Als hogere kosten leiden tot **hogere verkoopprijzen**, kann de vraag naar meubels verminderen. Dit dwingt meubelmakers tot **innovatie of kostenbesparing**. 
+
+    Een sterke **schommeling** zorgt voor **onzekerheid in kosten**. Dit heeft invloed op het margedruk, prijsbeleid en contractafspraken.
+
+            """)
